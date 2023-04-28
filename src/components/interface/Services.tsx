95955ae47d645746b274b9756e2db83a246a60a7
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-28 11:22:44 +0000

    Added Services  Section

--- src/components/interface/Services.tsx
+++ src/components/interface/Services.tsx
@@ -1,8 +1,109 @@
 import React from "react";
-import { Props } from "../types";
+import styled from "styled-components";
 
-const Services: React.FC<Props> = ({ children }) => {
-  return <div>Services</div>;
+const servicesInfo =
+  "At Nakia, we offer a comprehensive range of products and services that cater to diverse needs, encompassing Building and Maintenance Supplies, Janitorial Supplies, Cleaning Supplies, and Medical Courier Services. We are committed to fostering successful customer engagement and delivering exceptional experiences by cultivating robust client relationships, surpassing lifecycle requirements, and upholding our core values to the highest standards.";
+
+const ServicesSection = styled.section`
+  padding: 10px;
+  margin: 10px;
+
+  & {
+    text-align: center;
+    text-align: justify;
+  }
+  & h2 {
+    text-align: center;
+  }
+
+  & p {
+    padding: 10px;
+    margin: 10px;
+  }
+`;
+//TODO
+const ServiceGallery = styled.section``;
+
+const NAICSection = styled.section`
+  margin: 10px;
+  padding: 10px;
+
+  table {
+    border-collapse: collapse;
+    border: 2px solid rgb(200, 200, 200);
+    letter-spacing: 1px;
+    font-size: 0.8rem;
+  }
+
+  td,
+  th {
+    border: 1px solid rgb(190, 190, 190);
+    padding: 10px 20px;
+  }
+
+  th {
+    background-color: rgb(235, 235, 235);
+    text-align: center;
+    font-size: 20px;
+  }
+
+  td {
+    text-align: center;
+    font-weight: bold;
+  }
+`;
+const Services: React.FC = () => {
+  return (
+    <ServicesSection>
+      <h2>What we Offer</h2>
+      <p>{servicesInfo}</p>
+      {/* Services with images */}
+      <ServiceGallery></ServiceGallery>
+      <NAICSection>
+        <table>
+          <thead>
+            <tr>
+              <th>NAIC Codes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                423990 - Other Miscellaneous Durable Goods Merchant Wholesalers
+              </td>
+            </tr>
+            <tr>
+              <td>423220 - Home Furnishing Merchant</td>
+            </tr>
+            <tr>
+              <td>
+                423610 - Electrical Apparatus and Equipment, Wiring Supplies,
+                and Related Equipment Merchant Wholesalers
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                423450 - Medical, Dental, and Hospital Equipment and Supplies
+                Merchant
+              </td>
+            </tr>
+            <tr>
+              <td>339113 - Surgical Appliance & Supplies Manufacturing</td>
+            </tr>
+            <tr>
+              <td>
+                488999 - All Other Transit and Ground Passenger Transportation
+              </td>
+            </tr>
+            <tr>
+              <td>492210 - Local Messengers and Local Delivery</td>
+            </tr>
+          </tbody>
+        </table>
+      </NAICSection>
+    </ServicesSection>
+  );
 };
 
 export default Services;
