1e7279fb92d49261fd9452dc37b4e07350b48ba2
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-28 14:44:15 +0000

    Modified Body component

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Props } from "../types";
 const Body: React.FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+  return <main>{children}</main>;
 };
 
 export default Body;
