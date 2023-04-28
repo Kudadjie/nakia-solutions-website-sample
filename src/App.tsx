1542fbf25eb303b6a7eaf24f090e4cbff476adeb
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-28 10:57:04 +0000

    Fixed global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
+import Services from "./components/interface/Services";
+import Body from "./components/layout/Body";
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
-
+import GlobalStyle from "./globalStyles";
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
+      <Body>
+        <Services />
+      </Body>
     </>
   );
 }
