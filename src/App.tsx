b0c6b40fa636db0d4f6357483eb4c251dbc2e03d
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-28 14:44:38 +0000

    Included components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import AboutUs from "./components/interface/AboutUs";
+import ContactUs from "./components/interface/ContactUs";
 import Services from "./components/interface/Services";
 import Body from "./components/layout/Body";
+import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import GlobalStyle from "./globalStyles";
@@ -12,7 +15,10 @@ function App() {
       <Hero />
       <Body>
         <Services />
+        <AboutUs />
+        <ContactUs />
       </Body>
+      <Footer />
     </>
   );
 }
