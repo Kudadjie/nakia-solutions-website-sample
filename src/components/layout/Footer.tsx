76b34039e92ea2dc6aeb7e5bbba0a1cab902b1b2
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-28 14:43:24 +0000

    Modified footer component and added details

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Props } from "../types";
+import styled from "styled-components";
 
-const Footer: React.FC<Props> = () => {
-  return <div>Footer</div>;
+const FooterSection = styled.footer`
+  background-color: #252525;
+  padding: 10px;
+  color: #cacaca;
+  display: flex;
+  justify-content: center;
+`;
+const Footer: React.FC = () => {
+  return (
+    <FooterSection>
+      <p>© 2023 Nakia Solutions Incoporated</p>
+    </FooterSection>
+  );
 };
 
 export default Footer;
