f704bc8248801fca45a3bec49f470c6c1e4101b0
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-29 23:16:17 +0000

    Slight UI change

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import Logo from "../../assets/Logo.svg";
+
 //styled components
 const HeaderContainer = styled.header`
   display: flex;
-  align-items: center;
+  justify-content: center;
   padding: 15px;
   position: absolute;
   width: 100vw;
@@ -22,19 +23,11 @@ const HeaderContainer = styled.header`
 `;
 
 //react component
-
 const Header: React.FC = () => {
   return (
-    <>
-      <HeaderContainer>
-        <img
-          src={Logo}
-          alt="Nakia Solution Inc Logo"
-          width="150"
-          height="100"
-        />
-      </HeaderContainer>
-    </>
+    <HeaderContainer>
+      <img src={Logo} alt="Nakia Solution Inc Logo" width="150" height="100" />
+    </HeaderContainer>
   );
 };
 
