6911b20651237114051868803311615984be30c3
John David K.T. Kudadjie <johnkudadjiekabutetteh@outlook.com>
2023-04-29 23:36:32 +0000

    Added authorization

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,11 +6,18 @@ const FooterSection = styled.footer`
   padding: 10px;
   color: #cacaca;
   display: flex;
-  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  grid-row-gap: 5px;
 `;
 const Footer: React.FC = () => {
   return (
     <FooterSection>
+      <p>
+        Nakia Solutions Inc. is certified DBE, MBE by the Marland Department of
+        Transportation, Maryland. DUNS: 118546974; CAGE: 9A4J9
+      </p>
       <p>© 2023 Nakia Solutions Incoporated</p>
     </FooterSection>
   );
